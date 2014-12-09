bbaa57a0f0cf6008720ae39c722046d1e2938888
Duane Rosenbaum <duane06@gmail.com>
2014-12-09 16:46:38 -0700

    added GUI text and restart button, as well as restart script to end scene.

---
+++ Assets/Scripts/Restart.cs
@@ -0,0 +1,22 @@
+﻿/*
+File: Restart.cs
+Author: Duane Rosenbaum
+Date: December 9, 2014
+
+Manages the game GUI elements
+requires nothing
+applied to restart button.
+*/
+using UnityEngine;
+using System.Collections;
+
+public class Restart : MonoBehaviour {
+
+	void OnMouseDown()
+	{
+		Debug.Log("Clicked");
+
+		Application.LoadLevel("Opening Menu");
+	}//end mousedown
+
+}//end restart clasa
